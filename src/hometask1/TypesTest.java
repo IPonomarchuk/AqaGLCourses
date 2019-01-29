622f0097877e593eb9e7cbc59423dba60cb50ea9
IPonomarchuk <47009382+IPonomarchuk@users.noreply.github.com>
2019-01-29 22:39:18 +0200

    # HomeTask1 test commit

---
+++ src/hometask1/TypesTest.java
@@ -0,0 +1,25 @@
+package hometask1;
+
+public class TypesTest {
+
+    public static void main(String[] args) {
+
+        byte byteVar = 15;
+        short shortVar = 2;
+        int intVar = 3;
+        long longVar = 4l;
+        float floatVar = 5.55f;
+        double doubleVar = 6.66;
+        char charVar = 'c';
+        boolean booleanVar = true;
+
+        charVar = (char) byteVar;
+        System.out.println(charVar);
+
+
+
+
+
+    }
+
+}
